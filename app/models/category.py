12af9e7126436fe9a3aaf6e961d1ed52937cfb0c
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-03 21:35:29 +0500

    created model for categories and schemas

---
+++ app/models/category.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, Text, DateTime
+from datetime import datetime
+from app.core.database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20) ,unique=True)
+    description: Mapped[str] = mapped_column(String(100))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
---
+++ app/schemas/category.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+
+class CategoryBase(BaseModel):
+    """
+    Базовая модель категорий
+    """
+    name: str = Field(min_length=6, max_length=20, description='Название категории')
+    description: str = Field(min_length=1, max_length=100, description='Описание категории')
+
+class CategoryCreate(CategoryBase):
+    """
+    Модель для создания категории
+    """
+    pass
+
+class CategoryUpdate(CategoryBase):
+    """
+    Модель для обновления категории
+    """
+    name: str | None = Field(None, min_length=6, max_length=20)
+    description: str | None = Field(None, min_length=1, max_length=100)
+
+class CategoryResponse(CategoryBase):
+    """
+    Модель для ответа
+    """
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
